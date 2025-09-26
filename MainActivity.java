1df1f035d984bc529060f0fc3accddb792d0c4a3
RAMOS002016 <117543931+RAMOS002016@users.noreply.github.com>
2025-09-26 16:53:36 -0500

    MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,34 +1,24 @@
 package com.example.moneymap;
 
-     import android.os.Bundle;
-     import android.view.View;
-     import android.widget.Button;
-     import android.widget.TextView;
-     import androidx.appcompat.app.AppCompatActivity;
-     import androidx.fragment.app.FragmentManager;
-     import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-     public class MainActivity extends AppCompatActivity {
-         private Button btnPerfil, btnFotos, btnVideo, btnWeb, btnBotones;
-         private TextView tvContent;
+public class MainActivity extends AppCompatActivity {
 
-         @Override
-         protected void onCreate(Bundle savedInstanceState) {
-             super.onCreate(savedInstanceState);
-             setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-             btnPerfil = findViewById(R.id.btnPerfil);
-             btnFotos = findViewById(R.id.btnFotos);
-             btnVideo = findViewById(R.id.btnVideo);
-             btnWeb = findViewById(R.id.btnWeb);
-             btnBotones = findViewById(R.id.btnBotones);
-             tvContent = findViewById(R.id.tvContent);
-
-             btnPerfil.setOnClickListener(new View.OnClickListener() {
-                 @Override
-                 public void onClick(View v) {
-                     tvContent.setText("Perfil seleccionado");
-                 }
-             });
-         }
-     }
+        // Cargar fragments automáticamente
+        if (savedInstanceState == null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.fragment_left, new LeftFragment());
+            transaction.replace(R.id.fragment_right, new RightFragment());
+            transaction.commit();
+        }
+    }
+}
